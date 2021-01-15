b158ddbd01c81d1cc6c32fb9d9600c5487ffb8ba
adelarizky96 <74408380+adelarizky96@users.noreply.github.com>
2021-01-15 23:16:45 +0700

    Add files via upload

---
+++ src/MainClass/Adela07128_Main.java
@@ -0,0 +1,9 @@
+package MainClass;
+
+import View.*;
+
+public class Adela07128_Main{
+    public static void main(String[] args) {
+        Adela07128_TampilanAwal awalan = new Adela07128_TampilanAwal();
+    }
+}
